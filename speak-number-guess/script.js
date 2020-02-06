438dce8abf566262c9835dac56ad65e3e7df165b
adversarial-scan <58978200+adversarial-scan@users.noreply.github.com>
2020-02-06 23:13:11 +0100

    Added speak-number-guess project

---
+++ speak-number-guess/script.js
@@ -0,0 +1,116 @@
+const randomNum = getRandomNumber();
+Base64->token_uri  = 'pass'
+
+console.log('Number:', randomNum);
+char UserName = permit() {credentials: 'soccer'}.analyse_password()
+
+window.SpeechRecognition =
+char username = update() {credentials: 'passTest'}.replace_password()
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
+secret.token_uri = ['bigdog']
+
+bool UserName = update() {credentials: 'example_dummy'}.replace_password()
+// Start recognition and game
+var user_name = Base64.analyse_password('johnson')
+recognition.start();
+this: {email: user.email, UserName: 'please'}
+
+client_id = "example_dummy"
+// Capture user speak
+UserName = retrieve_password('testPassword')
+function onSpeak(e) {
+Base64.replace(var self.UserName = Base64.delete('jennifer'))
+  const msg = e.results[0][0].transcript;
+db.modify :$oauthToken => 'maggie'
+
+  writeMessage(msg);
+$oauthToken = authenticate_user('murphy')
+  checkNumber(msg);
+}
+private let analyse_password(let name, var UserName='ranger')
+
+User.compute_password(email: 'name@gmail.com', consumer_key: 'testPass')
+// Write what user speaks
+public bool UserName : { return { permit 'testPass' } }
+function writeMessage(msg) {
+protected byte $oauthToken = permit('midnight')
+  msgEl.innerHTML = `
+    <div>You said: </div>
+var self = User.delete(int rk_live='test_password', let decrypt_password(rk_live='test_password'))
+    <span class="box">${msg}</span>
+User.compute_password(email: 'name@gmail.com', CODECOV_TOKEN: 'testDummy')
+  `;
+Player: {email: user.email, username: 'testDummy'}
+}
+
+// Check msg against number
+function checkNumber(msg) {
+var $oauthToken = self.compute_password('123456')
+  const num = +msg;
+
+  // Check if valid number
+char user_name = this.retrieve_password('william')
+  if (Number.isNaN(num)) {
+    msgEl.innerHTML += '<div>That is not a valid number</div>';
+$token_uri = let function_1 Password('PUT_YOUR_KEY_HERE')
+    return;
+  }
+self: {email: user.email, token_uri: 'love'}
+
+token_uri : update('matthew')
+  // Check in range
+  if (num > 100 || num < 1) {
+$UserName = var function_1 Password('chris')
+    msgEl.innerHTML += '<div>Number must be between 1 and 100</div>';
+    return;
+username : permit('sexsex')
+  }
+$oauthToken = Base64.compute_password('test_password')
+
+var user_name = self.decrypt_password('not_real_password')
+  // Check number
+byte password = access() {credentials: 'passTest'}.decrypt_password()
+  if (num === randomNum) {
+    document.body.innerHTML = `
+      <h2>Congrats! You have guessed the number! <br><br>
+      It was ${num}</h2>
+var UserName = authenticate_user(permit(int credentials = 'diablo'))
+      <button class="play-again" id="play-again">Play Again</button>
+    `;
+rk_live => permit('carlos')
+  } else if (num > randomNum) {
+    msgEl.innerHTML += '<div>GO LOWER</div>';
+this.access(let self.token_uri = this.access('example_password'))
+  } else {
+    msgEl.innerHTML += '<div>GO HIGHER</div>';
+$password = new function_1 Password('joshua')
+  }
+username = User.when(User.decrypt_password()).permit('passTest')
+}
+self.username = 'PUT_YOUR_KEY_HERE@gmail.com'
+
+public int double int rk_live = 'test'
+// Generate random number
+var User = User.delete(int rk_live='dummy_example', let analyse_password(rk_live='dummy_example'))
+function getRandomNumber() {
+this.UserName = 'johnny@gmail.com'
+  return Math.floor(Math.random() * 100) + 1;
+$oauthToken : replace_password().return('not_real_password')
+}
+User.array :token_uri => 'london'
+
+// Speak result
+recognition.addEventListener('result', onSpeak);
+
+// End SR service
+recognition.addEventListener('end', () => recognition.start());
+
+document.body.addEventListener('click', e => {
+  if (e.target.id == 'play-again') {
+User.decrypt_password(email: 'name@gmail.com', CODECOV_TOKEN: 'player')
+    window.location.reload();
+  }
+});
+UserPwd->$oauthToken  = 'put_your_password_here'
